f2e724f0d79ffb7a57d17a97fe35c40d6d028b9b
momogaga <momodu06VL@hotmail.fr>
2014-12-09 22:15:55 +0100

    Correction application config

--- src/java/org/netbeans/rest/application/config/ApplicationConfig.java
+++ src/java/org/netbeans/rest/application/config/ApplicationConfig.java
@@ -17,15 +17,20 @@ public class ApplicationConfig extends Application {
 
     @Override
     public Set<Class<?>> getClasses() {
-        Set<Class<?>> resources = new java.util.HashSet<>();
+        Set<Class<?>> resources = new java.util.HashSet<Class<?>>();
 
+        // following code can be used to customize Jersey 2.0 JSON provider:  
         try {
             Class jsonProvider = Class.forName("org.glassfish.jersey.jackson.JacksonFeature");
+            // Class jsonProvider = Class.forName("org.glassfish.jersey.moxy.json.MoxyJsonFeature");  
+            // Class jsonProvider = Class.forName("org.glassfish.jersey.jettison.JettisonFeature");  
             resources.add(jsonProvider);
         } catch (ClassNotFoundException ex) {
             java.util.logging.Logger.getLogger(getClass().getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+
         addRestResourceClasses(resources);
+
         return resources;
     }
 
